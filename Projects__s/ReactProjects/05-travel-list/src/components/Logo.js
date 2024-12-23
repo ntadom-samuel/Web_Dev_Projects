4fb7a9599802d0d07627759b4e3b168d0ba588d8
ntadom-samuel <94174597+ntadom-samuel@users.noreply.github.com>
2024-12-23 08:57:19 -0600

    Delete Projects__s directory

--- Projects__s/ReactProjects/05-travel-list/src/components/Logo.js
+++
@@ -1,4 +0,0 @@
-//If we export with named export, when importing, the variable name must be the same as the exports name. With default exports, it can be any name
-export default function Logo() {
-  return <h1>🌴 Far Away 🧳</h1>;
-}
